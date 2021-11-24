fecca304f30ea91d5bfa5da3e85b336bc68dae92
unknown <contacto.acero.hernan@gmail.com>
2021-11-24 16:10:46 -0500

    header finished

--- src/views/Home/components/Hero/index.js
+++ src/views/Home/components/Hero/index.js
@@ -6,11 +6,14 @@ function Hero() {
   return (
     <div className={hero.container}>
       <div className={hero.image}></div>
-      <h1 className={hero.title}>Our big sale.</h1>
-      <span className={hero.subtitle}>
-        Early Black Friday has arrived. Get up to 50% ooff our entire colecction
-      </span>
-      <button className={hero.button}>See the deals.</button>
+      <div className={hero.textContainer}>
+        <h1 className={hero.title}>Our big sale.</h1>
+        <span className={hero.subtitle}>
+          Early Black Friday has arrived. Get up to 50% off our entire
+          colecction
+        </span>
+        <button className={hero.button}>See the deals.</button>
+      </div>
     </div>
   );
 }

5bda7e1e0100641e19c7fdb2399c2888a214071b
unknown <contacto.acero.hernan@gmail.com>
2021-11-24 11:55:04 -0500

    footer created

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,39 +2,43 @@ import React from "react";
 import { BiHappy as HappyIcon } from "react-icons/bi";
 import { AiOutlineLock as LockIcon } from "react-icons/ai";
 import { FiTruck as TruckIcon } from "react-icons/fi";
-import { MdPlayArrow as Arrow } from "react-icons/md";
-import { Link } from "react-router-dom";
+
+import FooterCard from "./components/FooterCard";
+import FooterNewsletter from "./components/FooterNewsletter";
+import FooterNav from "./components/FooterNav";
+import FooterSocialMedia from "./components/FooterSocialMedia";
 
 import footer from "./footer.module.css";
 
 function Footer() {
-  const FooterCard = ({ children }) => (
-    <div className={footer.card}>
-      {children}
-      <h3 className={footer.cardTitle}>Satisfaccion guaranteed</h3>
-      <span>
-        Easy returns within 30 days, <b>no questions asked</b>
-      </span>
-      <Link to="" className={footer.cardLink}>
-        Read more
-        <Arrow />
-      </Link>
-    </div>
-  );
-
   return (
     <div className={footer.container}>
       <div className={footer.cards}>
-        <FooterCard>
+        <FooterCard
+          title={"Satisfaccion guaranteed"}
+          msg={"Easy retunrs within 30 days,"}
+          msgBold={"no question asked"}
+        >
           <HappyIcon className={footer.icon} />
         </FooterCard>
-        <FooterCard>
+        <FooterCard
+          title={"All secure payment methods"}
+          msg={"Easy retunrs within 30 days,"}
+          msgBold={"no question asked"}
+        >
           <LockIcon className={footer.icon} />
         </FooterCard>
-        <FooterCard>
+        <FooterCard
+          title={"Worldwide delivery"}
+          msg={"Easy retunrs within 30 days,"}
+          msgBold={"no question asked"}
+        >
           <TruckIcon className={footer.icon} />
         </FooterCard>
       </div>
+      <FooterNewsletter />
+      <FooterNav />
+      <FooterSocialMedia />
     </div>
   );
 }
